c3497d9e5ba22d644cfa80b3ed4ea1a2952db5fe
dopamine <crack1933@gmail.com>
2022-01-14 13:17:19 +0700

    Revert "msm8996-common: Switch to android.hardware.power-service"
    This reverts commit 06a45695a879ed698376394b80195e28fbce75b3.

--- msm8996.mk
+++ msm8996.mk
@@ -314,7 +314,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service \
+    android.hardware.power-service-qti \
     vendor.qti.hardware.perf@1.0
 
 PRODUCT_COPY_FILES += \
