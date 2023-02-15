e87fa74240f9738ce8dff4a0813a922377f81751
dopamine <crack1933@gmail.com>
2023-02-15 14:11:29 +0700

    msm8996-common: try to fix SafetyNet

--- init/init_msm8996.cpp
+++ init/init_msm8996.cpp
@@ -30,18 +30,39 @@
  */
 
 #include <sys/sysinfo.h>
-
+#include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
 #include <android-base/properties.h>
 
 #include "property_service.h"
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using ::android::base::SetProperty;
 using android::init::property_set;
 
 char const *heapminfree;
 char const *heapmaxfree;
 char const *heaptargetutilization;
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_dual(char const system_prop[], char const vendor_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
 
 void check_device()
 {
@@ -83,4 +104,9 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("ro.oem_unlock_supported", "0");
+    property_override("ro.boot.verifiedbootstate", "green");
+    property_override("ro.boot.flash.locked", "1");
+    property_override("ro.boot.veritymode", "enforcing");
+    property_override("ro.boot.vbmeta.device_state", "locked");
 }
