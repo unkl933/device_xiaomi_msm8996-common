01eaf56850eae4568c9cae0b1d40ed8e7b687a03
dopamine <crack1933@gmail.com>
2023-02-16 00:34:45 +0700

    msm8996-common: replace configpanel from PE

--- configpanel/Android.mk
+++ configpanel/Android.mk
@@ -5,26 +5,21 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PACKAGE_NAME := ConfigPanel
-
 LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_REQUIRED_MODULES := XiaomiPocketMode
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-v7-appcompat \
-    android-support-v7-recyclerview
+LOCAL_USE_AAPT2 := true
 
-LOCAL_AAPT_FLAGS := \
-    --auto-add-overlay \
-    --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.recyclerview
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    androidx.core_core \
+    androidx.preference_preference
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/recyclerview/res
+    $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_PACKAGE)
